ff1b781ef6589cfc5d6aabcf3382611d064b2474
Kharid <kld22@njit.edu>
2018-04-30 18:15:28 -0400

    cleanup

--- html/login.php
+++ html/login.php
@@ -1,23 +1,17 @@
 <?php
-session_start();
-/* User login process, checks if user exists and password is correct */
 
-// Escape email to protect against SQL injections
+session_start();
+
 $user = $conn->escape_string($_POST['user']);
 $sql = "select * from user where user = '$user';";
 $result = $conn->query($sql);
 
-//var_dump($result);
-
 if ( $result->num_rows == 0 )
 { 
-    // User doesn't exist
-    echo "<br><strong>User Doesn't Exist!!</strong><br>".PHP_EOL;
-    
+    echo "<br><strong>User Doesn't Exist!!</strong><br>".PHP_EOL;  
 }
 else 
 { 
-    // User exists
     $sql = "select * from user where user = '$user';";
     $result = $conn->query($sql);
     
@@ -26,15 +20,11 @@ else
 
     if ($_POST['password'] == $userArray['pass']) 
     {   
-
-	$_SESSION['user'] = $user;
+	    $_SESSION['user'] = $user;
     	var_dump($user);
-      	// This is how we'll know the user is logged in	
-    	
-	$_SESSION['logged_in'] = true;
-
+        
+        $_SESSION['logged_in'] = true;
         header("location: tasker.php");
-	
     }
     else
     {
